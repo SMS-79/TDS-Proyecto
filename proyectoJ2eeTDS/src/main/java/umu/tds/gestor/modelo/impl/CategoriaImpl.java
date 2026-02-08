9d943649500054c75e1a7c4d9515382dfb4168be
PEYLAU <pedrochicocaballero@gmail.com>
2026-02-08 14:49:20 +0100

    impl_categoria_impl_gasto_parcial

--- proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/CategoriaImpl.java
+++ proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/CategoriaImpl.java
@@ -1,5 +1,35 @@
 package umu.tds.gestor.modelo.impl;
 
-public class CategoriaImpl {
+import umu.tds.gestor.modelo.Categoria;
 
+public class CategoriaImpl implements Categoria {
+	
+	private String nombre; //identificador de la categoría (atributo único)
+	
+	
+	//contrusctor vacío que es necesario para luego añadir la persistencia en JSON
+	public CategoriaImpl() {
+	}
+	
+	public CategoriaImpl(String nombre) {
+		this.nombre = nombre.trim(); //uso de trim para recibir cadena limpia de espacios
+	}
+	
+	@Override
+	public String getNombre() {
+		return nombre; 
+	}
+	
+	@Override
+	public void setNombre(String nombre) {
+		/*trim se usa para ignorar los espacios, 
+		 * así cubrimos el caso en el que se pueda introducir
+		   una cadena en blanco que sean solo espacios*/
+		if(nombre == null || nombre.trim().isEmpty()) {
+			throw new IllegalArgumentException("El nombre de la categoría no puede estar vacío."); 
+		}
+		this.nombre = nombre.trim(); //uso de trim para recibir cadena limpia de espacios
+	}
+	
+	
 }
--- proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/GastoImpl.java
+++ proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/GastoImpl.java
@@ -1,5 +1,84 @@
 package umu.tds.gestor.modelo.impl;
 
-public class GastoImpl {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import umu.tds.gestor.modelo.Gasto;
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property = "lista_gastos"
+		)
+
+
+public class GastoImpl implements Gasto{
+
+	private CategoriaImpl categoria; 
+	private LocalDate fecha; 
+	private double cantidad; 
+	private boolean activo; // para indicar que un gasto está activo
+	
+	@JsonProperty("lista_gastos")
+	private List<GastoImpl> gastos; 
+	
+	// constructor por defecto para mantener la persistencia en JSON
+	public GastoImpl() {
+	}
+	
+	public GastoImpl(CategoriaImpl categoria, LocalDate fecha, double cantidad) {
+		if (cantidad <= 0) {
+			throw new IllegalArgumentException("La cantidad debe de ser postiva.");
+		}
+		if(fecha == null) {
+			throw new IllegalArgumentException("La fecha no puede ser nula"); 
+		}
+		if(categoria == null) {
+			throw new IllegalArgumentException("La categoría no puede ser nula"); 
+		}
+		this.cantidad = cantidad; 
+		this.categoria = categoria; 
+		this.fecha = fecha; 
+	}
+	
+	public CategoriaImpl getCategoria() {
+		return categoria; 
+	}
+	
+	public LocalDate getFecha() {
+		return fecha; 
+	}
+	
+	public double getCantidad() {
+		return cantidad; 
+	}
+	
+	public void setFecha(LocalDate newFecha) {
+		if(fecha == newFecha) {
+			throw new IllegalArgumentException("La fecha debe ser distinta a la actual."); 
+		}
+		this.fecha = newFecha; 
+	}
+	
+	public void setCantidad(double newCantidad) {
+		this.cantidad = newCantidad; 
+	}
+	
+
+	public void setCategoria(CategoriaImpl newCategoria) {
+		if(this.categoria == newCategoria) {
+			throw new IllegalArgumentException("La categoria no puede ser la misma."); 
+		}
+		this.categoria = newCategoria; 
+	}
+	
+	public void borrar() {
+		this.activo = false; 
+	}
+	
+	
 }
