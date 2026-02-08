1e8e2845e32e3e481fc17ae93019c58aa15fca61
SMS-79 <salvamtnez79@gmail.com>
2026-02-08 15:14:24 +0100

    Añadido de implementacion de alerta y notificacion

--- proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/AlertaImpl.java
+++ proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/AlertaImpl.java
@@ -1,5 +1,41 @@
 package umu.tds.gestor.modelo.impl;
 
-public class AlertaImpl {
+import umu.tds.gestor.modelo.Alerta;
+import umu.tds.gestor.modelo.Categoria;
+import umu.tds.gestor.modelo.Intervalo;
 
+public class AlertaImpl implements Alerta {
+
+	private Categoria categoria;
+	private Intervalo intervalo;
+	private double limite;
+	
+	public AlertaImpl() {
+		
+	}
+	
+	public Categoria getCategoria() { 
+		return this.categoria;
+	}
+	
+	public Intervalo getIntervalo() { 
+		return this.intervalo;
+	}
+	
+	public double getLimite() { 
+		return this.limite;
+	}
+
+	public void setCategoria(Categoria categ) {
+		this.categoria = categ;
+	}
+
+	public void setIntervalo(Intervalo interv) {
+		this.intervalo = interv;
+		
+	}
+
+	public void setLimite(double lim) {
+		this.limite = lim;		
+	}
 }
--- proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/NotificacionImpl.java
+++ proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/NotificacionImpl.java
@@ -1,5 +1,24 @@
 package umu.tds.gestor.modelo.impl;
 
-public class NotificacionImpl {
+import java.util.Random;
+import java.util.UUID;
 
+import umu.tds.gestor.modelo.Notificacion;
+
+public class NotificacionImpl implements Notificacion {
+	
+	private String id;
+	private String mensaje;
+	
+	public NotificacionImpl(String m) {
+		this.id = UUID.randomUUID().toString();
+		this.mensaje = m;
+	}
+	
+	public String getId() { return this.id; }
+	
+	public String getMensaje() { return this.mensaje; }
+	
+	
+	
 }
