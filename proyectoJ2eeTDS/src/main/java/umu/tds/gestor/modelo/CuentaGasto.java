95766c1374c296a3676bfe12b587d18e5e06a087
Juan <juan.navarrof@um.es>
2026-02-24 13:09:17 +0100

    Persistencia cuentas

--- proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/CuentaGasto.java
+++ proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/CuentaGasto.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import umu.tds.gestor.modelo.impl.GastoImpl;
-
 public interface CuentaGasto {
 	
 	List<String> getParticipantes();
@@ -19,8 +17,6 @@ public interface CuentaGasto {
 	void setDistribucion(String nombre, double value);
 	
 	void setDistribuciones(Map<String, Double> distribuciones);
-	
-	void introducirGasto(String nombre, GastoImpl gasto);
 	
 	void recalcularSaldos();
 	
--- proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/CuentaGastoImpl.java
+++ proyectoJ2eeTDS/src/main/java/umu/tds/gestor/modelo/impl/CuentaGastoImpl.java
@@ -1,37 +1,54 @@
 package umu.tds.gestor.modelo.impl;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javafx.util.Pair;
 import umu.tds.gestor.modelo.CuentaGasto;
 import umu.tds.gestor.modelo.Gasto;
 
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property = "id_cuenta"
+		)
+
 public class CuentaGastoImpl implements CuentaGasto {
 
+	@JsonProperty("id_cuenta")
+	private UUID ID;
+	
 	//Tupla que asocia un nombre a un gasto. La persona que mete el gasto principal
 	private Pair<String, Gasto> gasto;
 	//Distribuiciones es para porcentajes, saldos es para valores enteros como tal
 	private Map<String, Double> distribuiciones;
 	private Map<String, Double> saldos;
 	private List<String> participantes;
-	//Identificador único de la cuenta
-	private UUID ID;
-	//Nombre a quien se le asocia el gasto original
 	
 	//Constructor vacio para la persistencia
 	public CuentaGastoImpl() {
 	}
 	
-	//TODO: Comprobacion de errores y persistencia
 	public CuentaGastoImpl(String... participantes) {
+		
+		if(Arrays.asList(participantes) == null || Arrays.asList(participantes).isEmpty()) {
+			throw new IllegalArgumentException("Se debe añadir al menos una persona a la cuenta");
+		}
+		
 		this.gasto = new Pair<String, Gasto>(null, null);
 		this.distribuiciones = new HashMap<String, Double>();
+		//El saldo de cada persona es un double y no un gasto, ya que puede ser -20, +10, etc, no necesita mas informacion
 		this.saldos = new HashMap<String, Double>();
-		this.participantes = Arrays.asList(participantes);
+		//Se hace asi para que sea modificable
+		this.participantes = new ArrayList<>(List.of(participantes));	
 		//Esto podria cambiarse para que haya repeticiones pero por el momento lo dejo así.
 		this.ID = UUID.randomUUID();
 		
@@ -39,7 +56,7 @@ public class CuentaGastoImpl implements CuentaGasto {
 			//100.0 entre la longitud de la lista para sacar porcentajes iguales
 			this.distribuiciones.put(p, 100.0/this.participantes.size());
 			//El saldo incial de cada persona será 0
-			this.saldos.put(p, null); //TODO: POner aqui gasto o algo, se verá mejor con gasto implementado
+			this.saldos.put(p, 0.0);
 		}
 	}
 	
@@ -66,7 +83,14 @@ public class CuentaGastoImpl implements CuentaGasto {
 		if(pagador == null || pagador.trim().isEmpty()) {
 			throw new IllegalArgumentException("El nombre del pagador no puede ser vacío");
 		}
+		if(!participantes.contains(pagador)) {
+			throw new IllegalArgumentException("El pagador debe ser un miembro de la cuenta");
+		}
 		this.gasto = new Pair<String, Gasto>(pagador, gasto);
+		this.saldos.put(pagador, gasto.getCantidad());
+		//Movemos el pagador al incio de la lista de participantes
+		this.participantes.remove(pagador);
+		this.participantes.add(0, pagador);
 		recalcularSaldos();
 	}
 
@@ -93,20 +117,12 @@ public class CuentaGastoImpl implements CuentaGasto {
 	}
 
 	@Override
-	public void introducirGasto(String nombre, GastoImpl gasto) {
-		// TODO Auto-generated method stub	
-	}
-
-	@Override
 	public void recalcularSaldos() {
+		
 		for(String p : this.participantes) {
-			//Multiplica el gasto total por el porcentaje que le corresponde a la persona
-			
-			//Los saldos se asocian a un double no a un gasto porque creo que no es necesario. Se podria cambiar si da problemas.
-			double gasto = this.gasto.getValue().getCantidad()*(this.distribuiciones.get(p)/100.0);
-			this.saldos.put(p, gasto);
-			
-			//TODO: HACER QUE SE CALCULEN EN NEGATIVO. ES DECIR, SI METO 30 EUROS QUE MI SALDO SEA +20 Y EL RESTO -10 O EQUIVALENTE.
+			//Al saldo actual de la persona se le resta su porcentaje asignado del gasto
+			double nuevoSaldo = saldos.get(p) - this.gasto.getValue().getCantidad()*(this.distribuiciones.get(p)/100.0);
+			this.saldos.put(p, nuevoSaldo);
 		}
 	}
 	
