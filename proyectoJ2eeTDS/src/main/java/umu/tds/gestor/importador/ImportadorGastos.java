42798d7645b0b96f18c2b0271ad5a8a0f17da35d
Davoc <jnf2004.jnf@gmail.com>
2026-04-06 12:38:06 +0200

    Añadida logica de creacion de gastos desde archivo

---
+++ proyectoJ2eeTDS/src/main/java/umu/tds/gestor/importador/ImportadorGastos.java
@@ -0,0 +1,20 @@
+package umu.tds.gestor.importador;
+
+import java.time.LocalDate;
+
+public abstract class ImportadorGastos {
+	
+	protected LocalDate fecha;
+	protected String cuenta;
+	protected String categoria;
+	protected String subcategoria;
+	protected String nota;
+	protected String pagador;
+	protected Double cantidad;
+	protected String divisa;
+	
+	protected ImportadorGastos() {}
+	
+	protected abstract void leerFichero(String path);
+
+}
